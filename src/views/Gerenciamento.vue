0178725000455accb6f15668a9e48c62187989c2
917rafael <rpilger@gmail.com>
2025-11-21 12:44:29 -0300

    feat: "footer" #5

--- src/views/Gerenciamento.vue
+++ src/views/Gerenciamento.vue
@@ -1,10 +1,12 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <script setup>
 import Header from '@/componente/header.vue'
+import Footer from '@/componente/footer.vue'
 </script>
 
 <template>
     <Header />
+    <Footer />
 </template>
 
 <style scoped>
