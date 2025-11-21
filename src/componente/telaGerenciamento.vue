4bd63c26940d4511577c959ce2f16c6762a688e4
ManoelFranciscoDias <manoel2007dias@gmail.com>
2025-11-21 14:14:39 -0300

    feat: criando tela de gerenciamento #8

---
+++ src/componente/telaGerenciamento.vue
@@ -0,0 +1,39 @@
+
+<script setup></script>
+
+<template>
+
+<div class="container">
+
+  <p>Imagem</p>
+  <p>Matricula</p>
+  <p>Acervo</p>
+  <p>Categoria</p>
+  <p>Materia-prima</p>
+  <p>Subtipo</p>
+  <p>Localização</p>
+  <p>Conservação</p>
+
+</div>
+
+</template>
+
+<style scoped>
+
+.container {
+  margin-top: 180px;
+  margin-left: 270px;
+  display: flex;
+  gap: 140px;
+  background-color: #FFE5D4;
+  width: 1578px;
+  height: 62px;
+  color: #C45D4C;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+
+</style>
+
