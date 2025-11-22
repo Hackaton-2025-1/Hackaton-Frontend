088932d2ba59cd55e78b556e9438863a73400c6c
higoramaral <amarahigor@gmail.com>
2025-11-22 02:59:40 -0300

    feat: Aplicando Nome e foto de usuario em controle de usuario e no NavBar. #27

--- src/views/GerenciamentoView.vue
+++ src/views/GerenciamentoView.vue
@@ -5,13 +5,14 @@ import telaGerenciamento from '@/componente/telaGerenciamento.vue';
 import telaView from '@/componente/telaView.vue';
 import { ref, onMounted } from 'vue';
 import { fetchArtefatos } from '../services/api.js';
+import { useUserStore } from '@/stores/user.js'
 
 const artefatos = ref([])
+const userStore = useUserStore()
 
 const loadArtefatos = async () => {
   artefatos.value = await fetchArtefatos()
 }
-
 
 onMounted(loadArtefatos)
 
@@ -27,7 +28,7 @@ onMounted(loadArtefatos)
     </div>
 
 
-    <NavBar></NavBar>
+    <NavBar :userSrc="userStore.avatar" :userName="userStore.name || 'Visitante'" />
     <SideBar></SideBar>
 
     <telaGerenciamento></telaGerenciamento>
